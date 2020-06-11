1a721adc9c9533b7b84d5feb3af486c2ee1ad5aa
reiyw <reiyw.setuve@gmail.com>
2020-06-11 14:00:44 +0900

    Mock client

--- tests/test_mirai_translate.py
+++ tests/test_mirai_translate.py
@@ -1,6 +1,23 @@
+import respx
+
 from mirai_translate import Client
 
+
+@respx.mock
 def test_client():
-    cli = Client()
+    trial = respx.get(
+        "https://miraitranslate.com/trial",
+        content=open("tests/fixtures/index.html").read(),
+    )
+    cli = Client(delay_sec=0)
+    assert trial.called
+    assert (
+        cli._tran == "jsXIHl5tgZHSJDgvPemaVRe9XIS1vME5eCoMOJfVJMN20TNpYaqTRu3PdvqzhVUm"
+    )
+
+    translate = respx.post(
+        "https://miraitranslate.com/trial/translate.php",
+        content={"status": "success", "outputs": [{"output": "This is a test."}]},
+    )
     assert cli.translate("これはテスト", "ja", "en") == "This is a test."
-    assert cli.translate("This is a test.", "en", "ja") == "これはテストです。"
+    assert translate.called
